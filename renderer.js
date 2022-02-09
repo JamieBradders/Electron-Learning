03f7b29b3098bda03f495d160284deb62f7f708d
Jamie Bradley <jamie@stackupdigital.co.uk>
2022-02-09 21:36:17 +0000

    Initial Commit

--- renderer.js
+++ renderer.js
@@ -4,3 +4,20 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+import marked from "marked";
+import { ipcRenderer } from "electron";
+
+let filePath = null;
+let originalContent = "";
+
+const markdownView = document.querySelector("#markdown");
+const htmlView = document.querySelector("#preview");
+
+const renderMarkdownToHtml = (markdown) => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+ipcRenderer.on("file-opened", (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
+});
